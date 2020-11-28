bf3d2245347025f91c8fe749895cdc6d52461bce
Hiba <hiba.mashfij@gmail.com>
2020-11-29 00:16:59 +0300

    added empty home

---
+++ src/containers/Home/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+// import Introduction from "../../components/Introduction";
+// import ProjectsList from "../../components/ProjectsList";
+import "./index.scss";
+
+const Home = () => {
+  return (
+    <div className="HomePage">
+      {/* <Introduction /> */}
+      {/* <ProjectsList /> */}
+    </div>
+  );
+};
+
+export default Home;
