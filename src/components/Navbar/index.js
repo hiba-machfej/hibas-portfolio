6bdcf639c20a7879b6b537c9ab17fb2b04a1f27f
Hiba <hiba.mashfij@gmail.com>
2020-11-29 00:14:19 +0300

    replaced Navbar

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,48 +1,18 @@
 import React from "react";
 import "./index.scss";
-import { Link } from "react-scroll";
+import { NavLink } from "react-router-dom";
 
 const Navbar = () => {
-  var Scroll = require("react-scroll");
-  var Events = Scroll.Events;
-
-  Events.scrollEvent.register("begin", function (to, element) {
-    console.log("begin", to, element);
-  });
-
   return (
     <div className="navbar">
-      Hiba Machfej
-      <Link
-        activeClass="active"
-        to="section1"
-        spy={true}
-        smooth={true}
-        offset={0}
-        duration={500}
-      >
-        project one
-      </Link>
-      <Link
-        activeClass="active"
-        to="section2"
-        spy={true}
-        smooth={true}
-        offset={0}
-        duration={500}
-      >
-        project two
-      </Link>
-      <Link
-        activeClass="active"
-        to="section3"
-        spy={true}
-        smooth={true}
-        offset={0}
-        duration={500}
-      >
-        project three
-      </Link>
+      <NavLink className="name" to="/">
+        <p>HIBA</p>
+        <p className="lastName">MACHFEJ</p>
+      </NavLink>
+      <div>
+        <NavLink to="section1">Projects</NavLink>
+        <NavLink to="section1">About</NavLink>
+      </div>
     </div>
   );
 };
