d67bedec109087dabaf0a28ec6eed3c83ea732ae
Hiba <hiba.mashfij@gmail.com>
2020-11-29 00:19:30 +0300

    added packages

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
 import React from "react";
+import "./App.scss";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
 import Home from "./containers/Home";
 
 function App() {
-  return <Home />;
+  return (
+    <Router>
+      <Navbar />
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
